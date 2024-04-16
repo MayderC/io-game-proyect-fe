6a50ec0c8d0a8e4792052807ecc3fa949615a0de
MayderC <maydercortes1@gmail.com>
2024-04-16 05:12:08 -0600

    Refactor socket event handling in on-event.ts

--- src/socket/on-event.ts
+++ src/socket/on-event.ts
@@ -5,13 +5,6 @@ import { State } from "../state/state";
 const instance = State.getInstance();
 const socket = instance.getSocket();
 
-export const onMovePlayer = () => {
-  socket.on(ON.MOVE, (data: { x: number; y: number; id: string }) => {
-    const player = instance.getPlayerById(data.id);
-    player?.move(data.x, data.y);
-  });
-};
-
 export const onAttack = () => {
   socket.on(ON.ATTACK_ACTION, (data: { player: Player }) => {
     const player = instance.getPlayerById(data.player.id);
